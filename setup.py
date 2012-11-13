a871916d86831d5f987fe2e4d2eff4b3dbb55c55
burke <mysql1230@gmail.com>
2012-11-13 19:38:55 +0800

    add setup.py for release build distribution

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='HelloWorld',
+      version='1.0',
+      description='A simple Python project',
+      author='Burke',
+      author_email='mysql1230@gmail.com',
+      url='https://github.com/gitstart/HelloWorld',
+      packages=['HelloWorld'],
+     )
+
